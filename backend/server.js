812c9dda9780771eb99547c7c05b73cbce0bb4c2
Asmi-aks08 <kit29.25bec013@gmail.com>
2026-09-19 14:04:00 +0530

    feat: add global error handler

--- backend/server.js
+++ backend/server.js
@@ -45,6 +45,15 @@ app.use((req, res) => {
     });
 });
 
+// Global error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+
+    res.status(500).json({
+        error: "Internal server error"
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`TalentFlow AI Backend running on port ${PORT}`);
 });
